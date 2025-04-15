b35da07a7d610fcefe29c24e20e71f5be89240ce
rahmani_saeed <s.rahmani.ce@gmail.com>
2025-04-15 10:54:28 +0330

    1404-02-26 v2

--- ap/livecodes/y404/m1/d26/Circle.java
+++ ap/livecodes/y404/m1/d26/Circle.java
@@ -1,6 +1,6 @@
 package ap.livecodes.y404.m1.d26;
 
-public class Circle {
+public class Circle extends Shape{
     private int r;
     private final double P = 3.14;
 
@@ -8,10 +8,16 @@ public class Circle {
         this.r=r;
     }
 
+    @Override
     public double getArea() {
 //        return 3.14*r*r;
 //        return P*r*r;
 //        return Math.PI*r*r;
         return Math.PI*Math.pow(r,2);
+    }
+
+    public int getR() {
+//        return r;
+        return this.r;
     }
 }
--- ap/livecodes/y404/m1/d26/Main.java
+++ ap/livecodes/y404/m1/d26/Main.java
@@ -1,10 +1,71 @@
 package ap.livecodes.y404.m1.d26;
 
-import ap.livecodes.y404m1d24.Rectangle;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
+//        main1(args);
+//        main2(args);
+        main3(args);
+    }
+
+    public static void main3(String[] args) {
+        Random random =new Random();
+
+        ArrayList<Shape> shapes= new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            shapes.add(new Rectangle(1+random.nextInt(100),1+ random.nextInt(100)));
+        }
+
+
+        for (int i = 0; i < 20; i++) {
+            shapes.add(new Circle(random.nextInt(100)));
+        }
+
+        double totalArea=0;
+        for (Shape shape : shapes) {
+            totalArea+=shape.getArea();
+        }
+        System.out.println("totalArea: "+totalArea);
+
+    }
+    public static void main2(String[] args) {
+        Random random =new Random();
+
+        ArrayList<Rectangle> rectangles= new ArrayList<>();
+
+        for (int i = 0; i < 20; i++) {
+            rectangles.add(new Rectangle(1+random.nextInt(100),1+ random.nextInt(100)));
+        }
+
+        ArrayList<Circle> circles= new ArrayList<>();
+
+        for (int i = 0; i < 1000; i++) {
+//            circles.add(new Circle(random.nextInt(100)));
+            Circle c = new Circle(1+random.nextInt(100));
+            circles.add(c);
+        }
+
+        double totalArea=0;
+        for (Rectangle rectangle : rectangles) {
+            totalArea+=rectangle.getArea();
+        }
+        for (Circle circle : circles) {
+            totalArea+=circle.getArea();
+        }
+        System.out.println("totalArea: "+totalArea);
+
+        for (int i = 0; i < circles.size(); i++) {
+            if(circles.get(i).getR()==0)
+                System.out.println("i: "+ i + " is zero");
+        }
+
+    }
+
+    public static void main1(String[] args) {
         Rectangle rectangle1=new Rectangle(10,12);
         Rectangle rectangle2=new Rectangle(2,3);
         Circle circle1 = new Circle(4);
